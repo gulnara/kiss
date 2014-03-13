5179953ca40268502b9b42262c53be7d65e28ed1
Gulnara Mirzakarimova <gulnara@gulnara.me>
2014-03-13 11:57:37 -0400

    half working ajax

--- app.rb
+++ app.rb
@@ -3,28 +3,28 @@ require './environments'
 require './actions/twitter'
 require 'json'
  
+
+def register(params)
+	puts "here is name for post #{params[:name]}"
+end
+
 get '/' do
+	@name = params[:name]
+	puts "here is name for get #{@name}"
 	@name ||= "kissmetrics"
  	@data = data(@name)
 	erb :"home.html"
-end
-
-def register(params)
-	@name = params[1]
 end
 
 post '/parse' do
 	register(params)
 end
 
-get '/parse' do
-	erb :"home.html"
-end
-
 def data(name)
+	puts "name of who we are searching for #{name}"
 	client = TwitterGrapher::SearchHelper.create
 	counts = client.count_tweets(@name)
   d_3 = []
   counts.each { |key, value| d_3 << { "label" => key , "value"=> value } }
-  @data = [{key: "Tweets for @#{@name}", values: d_3}]
+  @data = [{key: "Tweets for @#{name}", values: d_3}]
 end
