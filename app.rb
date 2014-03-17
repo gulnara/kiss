4fcd7ca154d1433dc650ef338055acaf514ee9ba
Gulnara Mirzakarimova <gulnara@gulnara.me>
2014-03-16 20:56:01 -0400

    testing mongomapper with heroku

--- app.rb
+++ app.rb
@@ -8,12 +8,21 @@ require 'json'
 
 include Mongo
 
-configure do
-	conn = MongoClient.new('localhost')
-	MongoMapper.connection = conn
-	MongoMapper.database = 'tweets'
-	require './models/tweet'
-end
+# configure do
+# 	conn = MongoClient.new('localhost')
+# 	MongoMapper.connection = conn
+# 	MongoMapper.database = 'tweets'
+# 	require './models/tweet'
+# end
+
+
+ 
+mongo_url = ENV['MONGOHQ_URL'] || 'mongodb://localhost/dbname'
+ 
+MongoMapper.connection = Mongo::Connection.from_uri mongo_url
+MongoMapper.database = URI.parse(mongo_url).path.gsub(/^\//, '') #Extracts 'dbname' from the uri
+require './models/tweet'
+
 
 get '/users' do
 	users = Mention.all
